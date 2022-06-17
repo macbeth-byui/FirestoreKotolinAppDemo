06bca508c1d31d6e428b9b78a6e42f7bfd5a87ea
macbeth-byui <macbethc@byui.edu>
2022-06-17 10:11:02 -0600

    Initial commit

---
+++ app/src/main/java/macbeth/firestorekotolinappdemo/MainActivity.kt
@@ -0,0 +1,88 @@
+package macbeth.firestorekotolinappdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var etName : EditText
+    private lateinit var etAge : EditText
+    private lateinit var etTitle : EditText
+    private lateinit var etUserName : EditText
+    private lateinit var lvUsers : ListView
+    private lateinit var lvUsersAdapter : ArrayAdapter<User>
+    private lateinit var db : FirebaseFirestore
+
+    private val users = mutableListOf<User>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        FirebaseApp.initializeApp(this)
+        db = Firebase.firestore
+
+
+        etName = findViewById(R.id.etName)
+        etAge = findViewById(R.id.etAge)
+        etTitle = findViewById(R.id.etTitle)
+        etUserName = findViewById(R.id.etUserName)
+        lvUsers = findViewById(R.id.lvUsers)
+
+        val bAdd = findViewById<Button>(R.id.bAdd)
+        bAdd.setOnClickListener { addUser() }
+
+        lvUsersAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, users)
+        lvUsers.adapter = lvUsersAdapter
+
+        registerFirestoreUpdates()
+    }
+
+    private fun addUser() {
+        val user = User(
+            etName.text.toString(),
+            etAge.text.toString().toInt(),
+            etTitle.text.toString())
+
+        db.collection("users").document(etUserName.text.toString())
+            .set(user)
+            .addOnSuccessListener { Log.d("FirestoreKotlinAppDemo", "New user sent to firestore: $user") }
+            .addOnFailureListener { error -> Log.d("FirestoreKotlinAppDemo", "Failed to send to firestore: $error") }
+    }
+
+    private fun registerFirestoreUpdates() {
+        val ref = db.collection("users")
+        // Register at the collection level which means I will receive everything when
+        // something changes in the collection.  A little wasteful, but a good place
+        // to start learning about how this works.
+        ref.addSnapshotListener { snapshot, error ->
+            if (error != null) {
+                Log.w("FirestoreKotlinAppDemo", "Error reading Firestore: $error")
+                return@addSnapshotListener
+            }
+            if (snapshot == null) {
+                Log.w("FirestoreKotlinAppDemo", "Snapshot was unexpectedly null.")
+                return@addSnapshotListener
+            }
+            // Clear out old users list so we can replace it
+            users.clear()
+            for (document in snapshot) {
+                val user = document.toObject<User>()
+                Log.d("FirestoreKotlinAppDemo", "Document in snapshot: $user")
+                users.add(user)
+            }
+            // Update the User Interface
+            lvUsersAdapter.notifyDataSetChanged()
+        }
+    }
+}
---
+++ app/src/main/java/macbeth/firestorekotolinappdemo/User.kt
@@ -0,0 +1,5 @@
+package macbeth.firestorekotolinappdemo
+
+data class User (val name : String? = null,
+                 val age : Int? = null,
+                 val title : String? = null)
